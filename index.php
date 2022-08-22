1e99e78fa956d402795094aeb8996ee26252d2a4
vtnboolean <vnthuanx@gmail.com>
2022-08-22 15:50:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<? /* PHP Badwords
+nome repo: php-badwords
+Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+
+*/
+
+$stringa;
+$censura='***';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <h1>PHP Badwords</h1>
+
+
+    <p>
+        <?
+        var_dump($_GET['badword']);
+        echo $stringa='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse unde voluptate doloremque sint sunt quod consequuntur quae quas, voluptatibus nesciunt voluptates pariatur tempora. Corrupti, aperiam mollitia! Nemo corrupti voluptas porro.';
+        
+        str_replace($_GET['badword'], $censura, $stringa);
+
+        ?>
+    </p>
+
+   
+    
+</body>
+</html>
